2ef885bdba05bafd50363e58752c7f236a74d6ae
itacamba <daniela.sahagun03@gmail.com>
2021-01-07 11:21:32 -0500

    search bar styling done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
 function App() {
+
+const [search, setSearch ] = useState('')
+const [movies, setMovies] = useState([])
+
+ const fetchMovies = (e) => {
+   e.preventDefault()
+   fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=be887243&s=${search}&type=movie`)
+   .then(resp => resp.json())
+   .then(data => {
+     let newMovies = data.Search.filter((mov, i) => i < 10 )
+     setMovies(newMovies)
+    // display error when no search input
+    // error handling when movie not found (try-catch)
+   })
+ } 
+ console.log(movies)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>The Shoppies Awards</h1>
+        <p>Nominate your top five favorite movies</p>
       </header>
+
+      <form action="" onSubmit={fetchMovies} id="search-form">
+          <p>Movie title</p>
+          <div className="search-bar">
+            <input type="text" name="movie-search" id="movie" placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value)} />
+            <input type="submit" value="Search"/>
+          </div>
+      </form>
+      <div className="results">
+        {movies.map((mov, i) => {
+          return <div key={i} className="movie">
+                    <h5>{mov.Title}</h5>
+                    <p>Released: {mov.Year}</p>
+                    <button className="nominate-btn">Nominate</button>
+                  </div>
+        })}
+      </div>
     </div>
   );
 }
